9feb727b1e816d89919e9d4245b3d544ffe40ecc
Yan Couto <yancouto@gmail.com>
2016-10-27 15:09:17 -0200

    cec

--- CF.cpp
+++ CF.cpp
@@ -14,6 +14,17 @@ inline ll mod(ll x) { return x % modn; }
 #	define debug(args...) fprintf(stderr, args)
 #endif
 
+template<class num> inline void read(num &x) {
+	char c;
+	while(isspace(c = getchar()));
+	bool neg = false;
+	if(!isdigit(c)) neg = (c == '-'), x = 0;
+	else x = c - '0';
+	while(isdigit(c = getchar()))
+		x = (x << 3) + (x << 1) + c - '0';
+	if(neg) x = -x;
+}
+
 int n;
 
 int main() {
