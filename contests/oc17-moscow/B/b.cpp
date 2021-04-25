bd551fbf80215e2fbcdbf723b155cec9f038b617
Yan Couto <yancouto@gmail.com>
2021-04-25 20:27:28 +0100

    Sorting out case-insensitive FS problems

--- contests/oc17-moscow/B/b.cpp
+++ contests/oc17-moscow/B/b.cpp
@@ -1,48 +1,69 @@
 #include <bits/stdc++.h>
+#define pb push_back
+
 using namespace std;
 
-int pr(int a){
-	for(int i=2;i*i<=a;i++)
-		if(a%i == 0)
-			return i;
-	return a;
+typedef long long ll;
+typedef pair<ll,ll> pii;
+
+const int N = 1000001;
+
+double rnd() { return rand() / double(RAND_MAX); }
+
+int f[N];
+int mx = -1;
+int pr[N];
+vector<int> nec[N];
+int est = 0;
+
+void go(double p) {
+	int cur = 0;
+	for(int i = 1; i < N; i++) {
+		if(pr[i] != i) {
+			f[i] = 1;
+			for(int p2 : nec[i])
+				f[i] = f[i] * f[p2];
+		} else f[i] = (rnd() <= p? 1 : -1);
+		cur += f[i];
+		if(abs(cur) > 20) {
+			mx = max(mx, i);
+			if(cur > 20) est++;
+			else est--;
+			return;
+		}
+	}
+	for(int i = 1; i < N; i++)
+		printf("%d ", f[i]);
+	putchar('\n');
+	exit(0);
 }
 
-int f[1001000];
-
-main(){
-	srand(0);
-	int n;
-	scanf("%d",&n);
-	int s = 0, ms = 0, Ms = 0;
-	
-	for(int i=1;i<=n;i++){
-		
-		if(i == 1)
-			f[i] = 1;
-		else if(pr(i) == i){
-			if(rand()%2)
-				f[i] = 1;
-			else
-				f[i] = -1;
+int main () {
+	srand(time(NULL));
+	int i, j;
+	vector<int> ps(1, 1);
+	pr[1] = 1;
+	for(i = 2; i < N; i++)
+		if(!pr[i]) {
+			ps.pb(i);
+			for(j = i; j < N; j += i)
+				pr[j] = i;
 		}
-		else {
-			f[i] = f[pr(i)] * f[i/pr(i)];
+	for(i = 2; i < N; i++) {
+		int x = i;
+		int prev = -1;
+		for(; x != 1; x /= pr[x]) {
+			if(prev == pr[x]) prev = -1;
+			else {
+				if(prev != -1) nec[i].pb(prev);
+				prev = pr[x];
+			}
 		}
-		
-		s += f[i];
-		
-		ms = min(ms,s);
-		Ms = max(Ms,s);
-		
-		if(abs(s) > 20){
-			printf("bad %d\n",i);
-			break;
-		}
-			
-	printf("%d %d\n",ms,Ms);
-		
-		printf("%d ",f[i]);		
 	}
-	
+	double p = 0;
+	scanf("%lf", &p);
+	for(i = 0; ; i++) {
+		if(!(i % 100000)) fprintf(stderr, "Test %d -- mx so far %d -- est %d\n", i, mx, est);
+		go(p);
+	}
 }
