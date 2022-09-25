16036921a9f8562cc2c55a968b0d6f266a7f2469
Yan Couto <yancouto@gmail.com>
2022-09-25 10:06:55 +0100

    1710

---
+++ Header.rs
@@ -0,0 +1,30 @@
+fn get_line() -> String {
+    let mut line = String::new();
+    while line.is_empty() {
+        std::io::stdin().read_line(&mut line).unwrap();
+    }
+    line
+}
+
+macro_rules! read_line {
+    ( $($name:ident : $typ:ty),+ ) => {
+        let line = get_line();
+        let mut tokens = line.split_ascii_whitespace();
+        $(
+            let $name: $typ = tokens.next().unwrap().parse().unwrap();
+        )+
+        debug_assert_eq!(tokens.next(), None);
+    };
+}
+
+fn read_line_vec<T>() -> Vec<T>
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    get_line()
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<T>())
+        .collect::<Result<Vec<T>, _>>()
+        .unwrap()
+}
